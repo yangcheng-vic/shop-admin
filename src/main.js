8d80460adfa005b30ebd900df7c377f06142c7e6
vic <1298504244@qq.com>
2019-06-28 11:31:05 +0800

    登录功能完成

--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui'
+
+import 'element-ui/lib/theme-chalk/index.css'
+import './assets/login.css'
+
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
+
+// 注册导航守卫
+router.beforeEach((to, from , next) => {
+  if (to.path === '/login' || to.path === '/') {
+    next()
+    return
+  }
+
+  if(localStorage.getItem("token")) {
+    next()
+  }else{
+    router.push("/login")
+  }
+})
 
 new Vue({
   router,
---
+++ src/views/login.vue
@@ -0,0 +1,95 @@
+<template>
+  <el-row type="flex" class="row-bg" justify="center" align="middle">
+    <el-col :xs="14" :sm="12" :md="10" :lg="8" :xl="6" class="login">
+      <el-form ref="loginForm" :model="loginForm" :rules="rules" label-width="80px" label-position="top">
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="loginForm.username"></el-input>
+        </el-form-item>
+
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="loginForm.password"></el-input>
+        </el-form-item>
+
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('loginForm')">登录</el-button>
+          <el-button @click="resetForm('loginForm')">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </el-col>
+  </el-row>
+</template>
+
+<style>
+  .row-bg {
+    height: 100%;
+    background-color: #2d434c;
+  }
+  .login {
+    background-color: #fff;
+    padding: 30px 20px;
+    border-radius: 10px;
+  }
+</style>
+
+
+<script>
+import { constants } from 'crypto'
+
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      loginForm: {
+        username: "",
+        password: ""
+      },
+      rules: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          {
+            min: 5,
+            max: 12,
+            message: "长度在 5 到 12 个字符",
+            trigger: "change"
+          }
+        ],
+        password: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          {
+            min: 6,
+            max: 12,
+            message: "长度在 6 到 12 个字符",
+            trigger: "change"
+          }
+        ]
+      }
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          axios({
+            method: "post",
+            data: this.loginForm,
+            url: "http://localhost:8888/api/private/v1/login"
+          }).then(({data: {data, meta}}) => {
+            console.log(data,meta)
+            if(meta.status === 200) {
+              localStorage.setItem("token", data.token)
+              this.$router.push("/home")
+            }
+          })
+        } else {
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
+  }
+};
+</script>
+
